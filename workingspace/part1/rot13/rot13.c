8da6c72ddf472a749525b9d121ef9d7396b28296
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-10-14 17:05:10 -0400

    Adding new rot13 and workspace folder

--- workingspace/part1/rot13/rot13.c
+++ workingspace/part1/rot13/rot13.c
@@ -1,24 +1,23 @@
 /*
  * rot13.c: ROT13 cipher test application
  *
- * Authors: Group Member 1 <email address>
- *          Group Member 2 <email address>
+@ Authors: Group Member 1: Arjun Ankleshwaria <aanklesh>
+@          Group Member 2: Jiten Mehta <jitenm>
+@		   Group Member 3: Ankit Chheda <achheda>
  * Date:    The current time & date
  */
 #include <unistd.h>
 #include <stdlib.h>
-#include <fileno.h>
 
 #define BUFSIZE 10
 
 int main(void)
 {
 	int i = 0;
-	int r, w = 0;
 	char buf[BUFSIZE];
 	while(1)
 	{
-		r = read(STDIN_FILENO, buf, BUFSIZE);
+		int r = read(0, buf, 15);
 		/* Read 0 bytes hence terminate the program */
 		if(r==0){
 			exit(0);
@@ -42,18 +41,10 @@ int main(void)
 				}
 			}
 		}
-	/* If Buffer overflows, set number of bytes to be written
-	 * equal to the BUFSIZE */
-		if( r > BUFSIZE)
-			r = BUFSIZE;
-
-		/* Ensuring that we retransmit in case of short count on write */
-		while(r!=w){
-			w = write(STDOUT_FILENO, buf, r);
-			if(w < 0)
-			{
-				exit(1);
-			}
+		int w = write(1, buf, r);
+		if(w < 0)
+		{
+			exit(1);
 		}
 	}
 	return 0;
