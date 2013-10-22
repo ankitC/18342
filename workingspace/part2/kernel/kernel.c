f58e9e3087cd5935cb70dfa6255ceb5eff0a0849
Ankit <achheda@localhost>
2013-10-22 08:16:02 -0400

    Wiring in handler changing 2 instrs

--- workingspace/part2/kernel/kernel.c
+++ workingspace/part2/kernel/kernel.c
@@ -6,13 +6,40 @@
  * Date:   The current time & date
  */
 
-//#include "utilfuncs.h"
- extern void setinterrput();
- extern void switch_to_user_mode();
+#include "kernel.h"
+
+extern void init(int argc, char** argv);
+extern void dispatcher();
+
+/*
+ * Variables to store the instructions of the
+ * original SWI handler
+ */
+unsigned* first_old_instr = 0;
+unsigned* second_old_instr = 0;
 
 int main(int argc, char *argv[]) {
-	/* Needs more code. */
 
-	return -255;
+	unsigned* swi_vector = (unsigned*) SWI_VECTOR_ADDR;
+	unsigned swi_loader = *swi_vector;
+	unsigned offset = swi_loader && (0xFFFFF000);
+	unsigned* old_SWI_addr = swi_vector + offset + 0x08;
+
+	/* Extracting the first 2 instructions */
+	first_old_instr = (unsigned*)	*(old_SWI_addr);
+	second_old_instr = (unsigned*) *(old_SWI_addr + 1);
+
+	/* Preparing instructions to substitute */
+	unsigned first_new_instr = (unsigned) LDR_PC_PC_minus_4;
+	unsigned second_new_instr = (unsigned) &dispatcher;
+
+	/* Wiring in our handler */
+	*old_SWI_addr = first_new_instr;
+	*(old_SWI_addr + 1) = second_new_instr;
+
+	init(argc, argv);
+
+	/* Need to restore the handler */
+	return 0;
 }
 
--- workingspace/part2/kernel/kernel.h
+++ workingspace/part2/kernel/kernel.h
@@ -3,5 +3,7 @@
 
 #define USR_STACK_BASE 0xa3000000
 #define USR_MAIN_ADDR 0xa2000000
+#define SWI_VECTOR_ADDR 0x08
+#define LDR_PC_PC_minus_4 0xe59ff000
 
 #endif
