5b89ee4b7ae469918c5ddb6a1417b3dcd29fc475
Ankit <achheda@localhost>
2013-10-22 13:47:00 -0400

    Remove redundant funcs, need to store the SWI_handler addr globally

--- workingspace/part2/kernel/kernel.h
+++ workingspace/part2/kernel/kernel.h
@@ -1,9 +1,12 @@
-#ifndef KERNEL_H
-#define KERNEL_H
+#ifndef _KERNEL_H
+#define _KERNEL_H
 
 #define USR_STACK_BASE 0xa3000000
 #define USR_MAIN_ADDR 0xa2000000
 #define SWI_VECTOR_ADDR 0x08
 #define LDR_PC_PC_minus_4 0xe59ff000
+
+//void init(int argc, char** argv);
+//void dispatcher();
 
 #endif
