57e35986a6fef20d0bd14163a77ce3bf584799ec
Arjun Ankleshwaria <aanklesh@andrew.cmu.edu>
2013-10-24 00:23:41 -0400

    working on restoring r8 and final return address after exit

--- workingspace/part1/testApp/testApp.c
+++ workingspace/part1/testApp/testApp.c
@@ -11,14 +11,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <bits/fileno.h>
-#include <exports.h>
+//#include <exports.h>
 
 int main(void)
 {
-//	char buf[11] = "hello MAC!\n";
-//	int r = read(0, buf, 11);
+	//char buf[11] = {};//"hello MAC!\n";
+	//int r = read(0, buf, 11);
 //	printf("read returned %d\n", r);
-	int w = write(1, "hello MAC!\n", 11);
-	printf("write returned %d\n", w);
+	int w = write(1, "hellO\n", 10);
+	//printf("write returned %d\n", w);
+	w=w;
+	exit(0);
 	return 0;
 }
--- workingspace/part2/kernel/C_SWI_handler.c
+++ workingspace/part2/kernel/C_SWI_handler.c
@@ -1,7 +1,15 @@
-#include<bits/fileno.h>
-#include<bits/errno.h>
+#include <bits/fileno.h>
+#include <bits/errno.h>
 #include <exports.h>
 
+/* Special characters */
+#define EOT				4
+#define BACKSPACE		8
+#define DELETE			127
+#define NEW_LINE		10
+#define CARRAIGE_RETURN	13
+
+extern void restore(void);
 //char *buf;
 void C_SWI_handler(int swino, unsigned* args)
 {
@@ -12,49 +20,59 @@ void C_SWI_handler(int swino, unsigned* args)
 
 			puts("Exit syscall called\n");
 			printf("exit returning %d\n", args[0]);
+			*old_SWI_addr = first_old_instr;
+			*(old_SWI_addr + 1) = second_old_instr;
+			restore();
 			break;
 
 		case(0x900003):
 			puts("Read syscall called\n");
-			if(args[0]==STDIN_FILENO)
+			if(args[0] == STDIN_FILENO)
 			{
 				int i;
 				for(i = 0; i < args [2]; i++)
 				{
 					char c = getc();
 					printf("read character: %c\n", c);
+
 					if(c == '\0')
 						break;
-					/* CLTR+D */
-					if(c == 4)
+
+					/* Ctrl + D */
+					if(c == EOT)
 						break;
-					/* BACKSPACE or DEL */
-					if( c == '\b' || c == 127)		//TODO delete encountered
+
+					/* Backspace or Delete */
+					if( c == BACKSPACE || c == DELETE)
 					{
-						buf[i-1]='\0';
 						i--;
+						buf[i]='\0';
 						puts("\b \b");
-						break;
+						continue;
 					}
-					/* NEW LINE or CARRAIGE RETURN */
-					if( c == '\n' || c == '\r')
+
+					/* New line or Carraige return */
+					if(c == NEW_LINE || c == CARRAIGE_RETURN)
 					{
 						buf[i] = c;
+						putc(buf[i]);
 						printf("read newline or carraige return: %c\n", c);
 						i++;
 						break;
 					}
 
+					/* For all other (normal) characters */
 					buf[i] = c;
+					putc(buf[i]);
 					printf("read: buf[i] = %c\n", buf[i]);
 				}
 
-				args[0] = i;
+				args[0] = i;	// return number of bytes read
 				printf("read returning %d\n", args[0]);
 			}
 			else
 			{
-				args[0] = -EBADF;
+				args[0] = -EBADF;	// return bad fd error
 				printf("read error: %d\n", args[0]);
 			}
 			break;
@@ -71,20 +89,23 @@ void C_SWI_handler(int swino, unsigned* args)
 						break;
 					putc(buf[i]);
 				}
-				args[0] = i;
+
+				args[0] = i;	// return number of bytes written
 				printf("write returning %d\n", args[0]);
 			}
 			else
 			{
-				args[0] = -EBADF;
+				args[0] = -EBADF;	// return bad fd error
 				printf("write error: %d\n", args[0]);
 			}
 			break;
 			//TODO EFAULT set
 
 		default:
+			/* return invalid SWI_number error */
 			puts("Invalid SWI no.\n");
 			args[0] =  -0xBADC0DE;
 			printf("default: swi no = %d\n", args[0]);
 	}
+	//TODO: return value = ssize_t?
 }
