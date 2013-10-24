2e7a8b289c244d0430142dcfe2d8de6dc9afecf0
Ankit <achheda@localhost>
2013-10-24 07:51:06 -0400

    Adding retore function, confirmed works

---
+++ workingspace/part2/kernel/restore.c
@@ -0,0 +1,21 @@
+/*
+ * kernel.c: Kernel main (entry) function
+ *
+ * Author: Group Member 1<email address>
+ *         Group Member 2 <email address>
+ * Date:   The current time & date
+ */
+
+#include "kernel.h"
+
+extern unsigned int *first_old_instr;
+extern unsigned int *second_old_instr;
+extern unsigned* old_SWI_addr;
+
+
+void restore(void)
+{
+	*old_SWI_addr =(unsigned) first_old_instr;
+	*(old_SWI_addr + 1) =(unsigned) second_old_instr;
+
+}
