b47af74622c0410287cde5b687b40fb986a076b8
Ankit <ankit.ac.chheda@gmail.com>
2013-10-28 15:47:51 -0400

    Adding the working starter code

--- lab3-support/kernel/main.c
+++ lab3-support/kernel/main.c
@@ -19,11 +19,31 @@ unsigned* old_SWI_addr = 0;
 
 unsigned* kernelsp = 0;
 
-int kmain(int argc, char** argv, uint32_t table)
+/*static uint32_t* prepare_user_stack(int argc, char** argv)
+{
+	uint32_t* stack_addr =(uint32_t*) USR_STACK_BASE;
+	int i = 0;
+	stack_addr--;
+	*stack_addr =(unsigned) ((void*)0);
+
+	for(i = argc - 1; i>=0; i--)
+	{
+		stack_addr--;
+		*stack_addr = (uint32_t)argv[i];
+	}
+
+	stack_addr--;
+	*stack_addr = (uint32_t)argc;
+
+	return stack_addr;
+}*/
+
+
+int kmain(int argc, char** argv, uint32_t table, uint32_t* stackp)
 {
 	app_startup(); /* bss is valid after this point */
 	global_data = table;
-
+	kernelsp = stackp;
 	/* Add your code here */
 	int retval = 0;
 	unsigned swi_dispatcher_addr =(unsigned) &dispatcher;
@@ -31,7 +51,11 @@ int kmain(int argc, char** argv, uint32_t table)
 	if((retval = hijack(swi_vector, swi_dispatcher_addr, old_SWI_addr, \
 					first_old_swii, second_old_swii)) == 0)
 		printf("SWI handler installation failed!!\n");
-	init(argc, argv);
 
+//	unsigned* user_stack_ptr = prepare_user_stack(argc, argv);
+//	init(user_stack_ptr);
+
+	init(argc, argv);
 	return 0;
 }
+
