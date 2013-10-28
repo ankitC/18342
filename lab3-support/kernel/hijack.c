5a79ef2a4bf62b04b71b0fdf4ed75395a70e4409
Ankit <ankit.ac.chheda@gmail.com>
2013-10-27 22:12:10 -0400

    Updating gitignore to not include binaries and fixing compiling problems in hijack

--- lab3-support/kernel/hijack.c
+++ lab3-support/kernel/hijack.c
@@ -10,8 +10,8 @@
 #include <exports.h>
 #include "kernel.h"
 
-int hijack(uint32_t vector_addr, uint32_t new_addr, uint32_t* first_old_instr,\
-	   	uint32_t* second_old_instr)
+int hijack(uint32_t vector_addr, uint32_t new_addr, uint32_t* old_addr, \
+		uint32_t* first_old_instr, uint32_t* second_old_instr)
 {
 
 	unsigned*  swi_vector = (unsigned int *) vector_addr;
@@ -20,19 +20,19 @@ int hijack(uint32_t vector_addr, uint32_t new_addr, uint32_t* first_old_instr,\
 
 	/* Address of the old handler */
 	unsigned*  old_SWI_container = (unsigned*)(vector_addr + offset + 0x08);
-	old_SWI_addr = (unsigned*) *old_SWI_container;
+	old_addr = (unsigned*) *old_SWI_container;
 
 	/* Extracting the first 2 instructions */
-	*first_old_instr =  *(old_SWI_addr);
-	*second_old_instr = *(old_SWI_addr + 1);
+	*first_old_instr =  *(old_addr);
+	*second_old_instr = *(old_addr + 1);
 
 	/* Preparing instructions to substitute */
 	unsigned int first_new_instr = (unsigned int) LDR_PC_PC_minus_4;
 	unsigned int second_new_instr = new_addr;
 
 	/* Wiring in our handler */
-	*old_SWI_addr = first_new_instr;
-	*(old_SWI_addr + 1) = second_new_instr;
+	*old_addr = first_new_instr;
+	*(old_addr + 1) = second_new_instr;
 
 	return 1;
 }
