fd4bd689123f35ac616a473d4ba7f1c5b278da5d
Arjun Ankleshwaria <arjun.a2812@gmail.com>
2013-11-02 00:45:26 -0400

    for jiten.. adding new script and splat.c

--- lab3-support/tasks/splat/splat.c
+++ lab3-support/tasks/splat/splat.c
@@ -3,10 +3,28 @@
  * @brief Displays a spinning cursor.
  *
  * Links to libc.
+ *
+ * Authors: Group Member 1: Arjun Ankleshwaria <aanklesh>
+ *		   Group Member 2: Jiten Mehta <jitenm>
+ *		   Group Member 3: Ankit Chheda <achheda>
+ *
  */
+
+#include <bits/fileno.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
+	while(1)
+	{
+		write(STDOUT_FILENO, "|\r", 2);
+		//sleep(2000);
+		write(STDOUT_FILENO, "/\r", 2);
+		//sleep(2000);
+		write(STDOUT_FILENO, "-\r", 2);
+		//sleep(2000);
+		write(STDOUT_FILENO, "\\\r", 2);
+		//sleep(2000);
+	}
 	return 0;
 }
