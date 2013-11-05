852a4cf337919deca8943ec43fa8138749590737
Ankit <ankit.ac.chheda@gmail.com>
2013-11-05 12:56:31 -0500

    Changing the timer implementation

--- lab3-support/kernel/C_IRQ_handler.c
+++ lab3-support/kernel/C_IRQ_handler.c
@@ -13,23 +13,23 @@
 
 extern unsigned long timer_counter;
 
-static void handle_timer_irq(void)
+/*static void handle_timer_irq(void)
 {
 //	printf(" %lu \n",timer_counter);
+	uint32_t clocks_till_interrupt = 0;
 	timer_counter++;
-	reg_write(OSTMR_OSCR_ADDR, 0x0);
 
+	clocks_till_interrupt = (OSTMR_FREQ*TIMER_RESOLUTION)/1000;
+	reg_write(OSTMR_OSMR_ADDR(0), clocks_till_interrupt);
 	return;
-}
+}*/
 
 void C_IRQ_handler()
 {
-	uint32_t icpr_reg, osmr0_mask, ossr_reg;
+	uint32_t icpr_reg, osmr0_mask;
+	uint32_t clocks_till_interrupt = 0;
 	icpr_reg = reg_read(INT_ICIP_ADDR);
 
-	/*
-	 *          * if the source is not osmr0 == oscr, bail out
-	 *                   */
 	osmr0_mask = 0x1 << INT_OSTMR_0;
 
 	/* Check if the interrupt was from timer */
@@ -38,11 +38,14 @@ void C_IRQ_handler()
 		return;
 	}
 
-	handle_timer_irq();
+	timer_counter++;
+	clocks_till_interrupt = reg_read(OSTMR_OSMR_ADDR(0));
+	clocks_till_interrupt += (OSTMR_FREQ*TIMER_RESOLUTION)/1000;
+	reg_write(OSTMR_OSMR_ADDR(0), clocks_till_interrupt);
 
-	ossr_reg = reg_read(OSTMR_OSSR_ADDR);
-	ossr_reg |= OSTMR_OSSR_M0;
-	reg_write(OSTMR_OSSR_ADDR, ossr_reg);
+//	ossr_reg = reg_read(OSTMR_OSSR_ADDR);
+//	ossr_reg |= OSTMR_OSSR_M0;
+//	reg_write(OSTMR_OSSR_ADDR, ossr_reg);
 
 	//ossr_reg = reg_read(OSTMR_OSSR_ADDR);
 	return;
