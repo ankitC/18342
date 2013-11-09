edf70c5f509436100266f5b7d56395b17f058011
Ankit <ankit.ac.chheda@gmail.com>
2013-11-09 04:09:49 -0500

    Adding IRQ methods to go back to prev instr

--- lab3-support/kernel/C_IRQ_handler.c
+++ lab3-support/kernel/C_IRQ_handler.c
@@ -13,17 +13,6 @@
 
 extern unsigned long timer_counter;
 
-/*static void handle_timer_irq(void)
-{
-//	printf(" %lu \n",timer_counter);
-	uint32_t clocks_till_interrupt = 0;
-	timer_counter++;
-
-	clocks_till_interrupt = (OSTMR_FREQ*TIMER_RESOLUTION)/1000;
-	reg_write(OSTMR_OSMR_ADDR(0), clocks_till_interrupt);
-	return;
-}*/
-
 void C_IRQ_handler()
 {
 	uint32_t icpr_reg, osmr0_mask, ossr_reg;
@@ -33,10 +22,10 @@ void C_IRQ_handler()
 	osmr0_mask = 0x1 << INT_OSTMR_0;
 
 	/* Check if the interrupt was from timer */
-	if(!(icpr_reg & osmr0_mask)) {
+/*	if(!(icpr_reg & osmr0_mask)) {
 		printf("\n Interrupt from an unknonwn source.\n");
 		return;
-	}
+	}*/
 
 	timer_counter++;
 	clocks_till_interrupt = reg_read(OSTMR_OSMR_ADDR(0));
@@ -47,7 +36,7 @@ void C_IRQ_handler()
 	ossr_reg |= OSTMR_OSSR_M0;
 	reg_write(OSTMR_OSSR_ADDR, ossr_reg);
 
-	//ossr_reg = reg_read(OSTMR_OSSR_ADDR);
+	ossr_reg = reg_read(OSTMR_OSSR_ADDR);
 	return;
 }
 
