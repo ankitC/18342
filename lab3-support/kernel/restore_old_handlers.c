5631540a899c520540ef47e4bbb94ef8c0d67a38
Ankit <ankit.ac.chheda@gmail.com>
2013-11-09 04:12:37 -0500

    Method to restore mode handlers

--- lab3-support/kernel/restore_old_handlers.c
+++ lab3-support/kernel/restore_old_handlers.c
@@ -7,28 +7,40 @@
  */
 
 #include "kernel.h"
+#include <exports.h>
+extern unsigned int first_old_swii;
+extern unsigned int second_old_swii;
 
-extern unsigned int *first_old_swii;
-extern unsigned int *second_old_swii;
-extern unsigned* old_SWI_addr;
-
-extern unsigned int *first_old_irqi;
-extern unsigned int *second_old_irqi;
-extern unsigned* old_IRQ_addr;
+extern unsigned int first_old_irqi;
+extern unsigned int second_old_irqi;
 
 
 /*
  * Restoring the original SWI_Handler
  * so that we don't break anything :D
  */
-void restore_old_SWI_h(void)
+void restore_old_handlers(void)
 {
-	*old_SWI_addr =(unsigned) first_old_swii;
-	*(old_SWI_addr + 1) =(unsigned) second_old_swii;
-}
+	uint32_t temp = SWI_VECTOR_ADDR;
+	uint32_t* swi_vector = (uint32_t*) temp;
+	uint32_t  swi_loader = *swi_vector;
+	uint32_t  offset = swi_loader & (0x0FFF);
 
-void restore_old_IRQ_h(void)
-{
-	*old_IRQ_addr =(unsigned) first_old_irqi;
-	*(old_IRQ_addr + 1) =(unsigned) second_old_irqi;
+	/* Address of the old handler */
+	uint32_t*  old_SWI_container = (uint32_t*)( SWI_VECTOR_ADDR + offset + 0x08);
+	uint32_t* old_addr = (uint32_t*) *old_SWI_container;
+
+	*old_addr = first_old_swii;
+	*(old_addr + 1) = second_old_swii;
+
+	temp = IRQ_VECTOR_ADDR;
+	swi_vector = (uint32_t*) temp;
+	swi_loader = *swi_vector;
+	offset = swi_loader & (0x0FFF);
+	old_SWI_container = (uint32_t*)(IRQ_VECTOR_ADDR + offset + 0x08);
+
+
+	old_addr = (uint32_t*) *old_SWI_container;
+	*old_addr = first_old_irqi;
+	*(old_addr + 1) = second_old_irqi;
 }
