a248de848fa054867d62c5e82f1783438c6c69fc
Ankit <ankit.ac.chheda@gmail.com>
2013-11-17 16:39:10 -0500

    Adding mutex lock and unlock functions

--- lab4/kernel/lock/mutex.c
+++ lab4/kernel/lock/mutex.c
@@ -9,7 +9,7 @@
  * @date  
  */
 
-//#define DEBUG_MUTEX
+#define DEBUG_MUTEX
 
 #include <lock.h>
 #include <task.h>
@@ -19,7 +19,10 @@
 #include <arm/exception.h>
 #ifdef DEBUG_MUTEX
 #include <exports.h> // temp
+#define DEBUG
 #endif
+
+#define null (void*) 0
 
 mutex_t gtMutex[OS_NUM_MUTEX];
 
@@ -28,10 +31,10 @@ void mutex_init()
 	int i = 0;
 	for ( i =0 ;i< OS_NUM_MUTEX ; i++)
 	{
-		gtMutex[i].bAvailable = true;
-		gtMutex[i].pHolding_Tcb = NULL;
-		gtMutex[i].bLock = false;
-		gtMutex[i].pSleep_queue = NULL;
+		gtMutex[i].bAvailable = TRUE;
+		gtMutex[i].pHolding_Tcb = (void*) 0;
+		gtMutex[i].bLock = FALSE;
+		gtMutex[i].pSleep_queue = (void*) 0;
 	}
 }
 
@@ -40,9 +43,9 @@ int mutex_create(void)
 	int i = 0 ;
 	for (i = 0; i<OS_NUM_MUTEX ; i++)
 	{
-		if(gtMutex[i].bAvailable == true )
+		if(gtMutex[i].bAvailable == TRUE )
 		{
-			gtMutex[i].bAvailable = false;
+			gtMutex[i].bAvailable = FALSE;
 			return i; // fix this to return the correct value
 
 		}
@@ -53,18 +56,81 @@ int mutex_create(void)
 
 int mutex_lock(int mutex  __attribute__((unused)))
 {
-	if(gtMutex[mutex].bAvailable == true || mutex >= OS_NUM_MUTEX)
+	tcb_t *cur = get_cur_tcb();
+	tcb_t *next = gtMutex[mutex].pSleep_queue;
+
+	/* Error checks */
+	if(gtMutex[mutex].bAvailable == TRUE || mutex >= OS_NUM_MUTEX || mutex < 0)
 		return EINVAL;
-	
-	if(gtMutex[mutex].bLock == true )
+
+	/* If you already own the mutex, it will cause a deadlock*/
+	if(cur == gtMutex[mutex].pHolding_Tcb)
 		return EDEADLOCK;
-	
-	gtMutex[mutex].pHolding_Tcb = get_cur_tcb();
-	return 1; // fix this to return the correct value
+#ifdef DEBUG
+	printf("Trying to allocated mutex to %u.\n", cur->native_prio);
+#endif
+	/* If someone is already holding the mutex and its not yourself */
+	if(gtMutex[mutex].pHolding_Tcb!= null)
+	{
+		/* Remove the current from run queue and add it to the sleep queue */
+		if(cur != runqueue_remove(cur->cur_prio))
+			printf("Couldn't remove current during mutex lock.\n");
+		
+		cur->sleep_queue = null;
+		
+		if(gtMutex[mutex].pSleep_queue == null)
+			gtMutex[mutex].pSleep_queue = cur;
+			
+		else/* Find the last element on the sleep queue.*/
+		{
+			while(next->sleep_queue != null)
+				next = next->sleep_queue;
+
+			next->sleep_queue = cur;
+		}
+
+		#ifdef DEBUG
+		printf("Mutex unavail for %u. Task %u holds it.\n", cur->native_prio, gtMutex[mutex].pHolding_Tcb->native_prio);	
+		#endif
+			//@TODO: Schedule the next task and put the current to sleep.
+			//dispatch_sleep();
+	}
+
+	gtMutex[mutex].pHolding_Tcb = cur;
+	gtMutex[mutex].bLock = TRUE;
+	cur->holds_lock++;
+#ifdef DEBUG
+	printf("Mutex allocated to task %u.\n", cur->native_prio);
+#endif
+	return 0; // fix this to return the correct value
 }
 
 int mutex_unlock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	tcb_t *cur = get_cur_tcb();
+	tcb_t *head = gtMutex[mutex].pSleep_queue;
+
+	if(gtMutex[mutex].bAvailable == TRUE || mutex >= OS_NUM_MUTEX || mutex < 0)
+		return EINVAL;
+
+	/* If you do not hold the mutex but cal an unlock on it, then Invalid!!*/
+	if(cur != gtMutex[mutex].pHolding_Tcb)
+		return EPERM;
+
+	cur->holds_lock--;
+	gtMutex[mutex].pHolding_Tcb = null;
+	gtMutex[mutex].bLock = FALSE;
+	//head = gtMutex[mutex].pSleep_queue;
+
+	if(head != null)
+	{
+		gtMutex[mutex].pSleep_queue = head->sleep_queue;
+		head->sleep_queue=null;
+		runqueue_add(head, head->cur_prio);
+	}
+#ifdef DEBUG
+	printf("Mutex deallocated from task %u.\n", cur->native_prio);
+#endif
+	return 0; // fix this to return the correct value
 }
 
