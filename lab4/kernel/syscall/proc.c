61f9089507503a2b52439f1f53c038a9c1252275
parallels <parallels@JitenMacBook.(none)>
2013-11-18 01:39:56 -0500

    Added checks to task_create

--- lab4/kernel/syscall/proc.c
+++ lab4/kernel/syscall/proc.c
@@ -22,14 +22,37 @@
 #include <arm/physmem.h>
 #include <device.h>
 
+#define START_ADD 0xa0000000
+#define BOUND 0xa3000000
+#define null (void*)0
+
 int task_create(task_t* tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
 {
-  return 1; /* remove this line after adding your code */
+	//return 1;  remove this line after adding your code 
+	int i,j = 0;
+	if(num_tasks > 64)
+		return -EINVAL;//TODO set EINVAL
+
+	for(i = 0 ; (unsigned)i < num_tasks ; i++)
+	{
+		if(tasks[i].lambda == null)
+			return -EINVAL;//TODO Function is null
+
+		for(j=0 ; (unsigned)j < num_tasks ; j++)
+		{
+			if(tasks[i].stack_pos == tasks[j].stack_pos && i !=j)
+				return -EINVAL;//TODO Function has same stack position as some other task
+		}
+
+		if(!(valid_addr(tasks[i].stack_pos, sizeof(tasks[i]), START_ADD, BOUND)))
+				return -EFAULT;//TODO Invalid Memory location EFAULT
+	}
+	return 1; //TODO, put to avoid warning
 }
 
 int event_wait(unsigned int dev  __attribute__((unused)))
 {
-  return 1; /* remove this line after adding your code */	
+	return 1; /* remove this line after adding your code */	
 }
 
 /* An invalid syscall causes the kernel to exit. */
