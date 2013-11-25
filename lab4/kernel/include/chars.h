d67a6238a03eeeb1a0db11c30373bb31e2066315
Arjun Ankleshwaria <aanklesh@andrew.cmu.edu>
2013-11-25 01:20:33 -0500

    created new header chars.h for special characters used in read and write syscalls

---
+++ lab4/kernel/include/chars.h
@@ -0,0 +1,21 @@
+/** @file chars.h
+ *
+ * @brief A set of special characters.
+ *
+ * @author Arjun Ankleshwaria <aanklesh>
+ *         Jiten Mehta <jitenm>
+ *         Ankit Chheda <achheda>
+ *
+ * @date Nov. 16, 2013  6:00 pm
+ */
+
+#ifndef _CHARS_H_
+#define _CHARS_H_
+
+#define EOT					0x04
+#define BACKSPACE			0x08
+#define DELETE				0x7f
+#define NEW_LINE			0x0a
+#define CARRAIGE_RETURN		0x0d
+
+#endif /* _CHARS_H_ */
